4225fa67e48c7b199dce45dd6c99c81e2bd631b0
Jotamontiel <jorgemontielmontiel@gmail.com>
2021-01-11 19:00:22 -0300

    Add new city3d background, cmd simulation and music, modify the services

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import HomePageView
+from . import views
 
 urlpatterns = [
     path('', HomePageView.as_view(), name="home"),
+    path('cmd_display/', views.cmdDisplayView, name="cmd_display"),
+    path('city3D_display/', views.city3DDisplayView, name="city3D_display"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,14 @@
 from django.views.generic.base import TemplateView
 from django.shortcuts import render
+from django.views.decorators.clickjacking import xframe_options_sameorigin
 
 class HomePageView(TemplateView):
     template_name = "core/home.html"
+
+@xframe_options_sameorigin
+def cmdDisplayView(request):
+    return render(request, "core/cmd_display.html")
+
+@xframe_options_sameorigin
+def city3DDisplayView(request):
+    return render(request, "core/city3D_display.html")
