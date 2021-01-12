0f95f9ef29596a9396ddcd79a8cc1f7e3c7f37a4
Jotamontiel <jorgemontielmontiel@gmail.com>
2021-01-12 14:53:53 -0300

    Add new view info, also add display advice and timeline, modify urls and views

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, InfoPageView
 from . import views
 
 urlpatterns = [
     path('', HomePageView.as_view(), name="home"),
+    path('info/', InfoPageView.as_view(), name="info"),
     path('cmd_display/', views.cmdDisplayView, name="cmd_display"),
     path('city3D_display/', views.city3DDisplayView, name="city3D_display"),
+    path('timeLine_display/', views.timeLineDisplayView, name="timeLine_display"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,9 +1,19 @@
 from django.views.generic.base import TemplateView
 from django.shortcuts import render
 from django.views.decorators.clickjacking import xframe_options_sameorigin
+import random
 
 class HomePageView(TemplateView):
     template_name = "core/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        context['choice_song'] = random.randrange(2)
+
+        return context
+
+class InfoPageView(TemplateView):
+    template_name = "core/info.html"
 
 @xframe_options_sameorigin
 def cmdDisplayView(request):
@@ -12,3 +22,7 @@ def cmdDisplayView(request):
 @xframe_options_sameorigin
 def city3DDisplayView(request):
     return render(request, "core/city3D_display.html")
+
+@xframe_options_sameorigin
+def timeLineDisplayView(request):
+    return render(request, "core/timeline_display.html")
