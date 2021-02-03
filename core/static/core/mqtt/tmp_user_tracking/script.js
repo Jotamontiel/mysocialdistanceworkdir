10477f9d296e36e9404c123d238ee8d3bb92f161
Jotamontiel <jorgemontielmontiel@gmail.com>
2021-02-03 16:47:35 -0300

    Add javascript tracking for website

--- core/static/core/mqtt/tmp_user_tracking/script.js
+++ core/static/core/mqtt/tmp_user_tracking/script.js
@@ -1,6 +1,14 @@
-/*
-var tmpClientUser = 'Client_user_'+ Math.round(Math.random() * (0-10000) * -1);
-
+var url = window.location.href;
+var match = url.match(/tmpClient_(\d+)/);
+if (match) {
+    var tmpClientUser = 'tmpClient_'+ match[1];
+} else {
+    var tmpClientUser = 'tmpClient_'+ Math.round(Math.random() * (0-10000) * -1);
+}
+function addTmpUserToLink(id) {
+    var anchor = document.getElementById(id);
+    anchor.href = anchor.href+"?"+tmpClientUser;
+}
 const options = {
     clean: true,
     connectTimeout: 4000,
@@ -9,10 +17,8 @@ const options = {
     password: 'jorgeuser3',
     // keepalive: 60,
 }
-
 const connectUrl = 'wss://socialdistance.cl:8084/mqtt'
 const client = mqtt.connect(connectUrl, options)
-
 client.on('connect', () => {
     console.log('Mqtt conectado por WSS! Exito!')
     client.subscribe('conn_channel', { qos: 0 }, (error) => {
@@ -22,20 +28,19 @@ client.on('connect', () => {
             console.log('Suscripción a conn_channel fallida!')
         }
     })
-    client.publish('conn_channel', 'He detectado un usuario temporal llamado '+tmpClientUser+' en tu plataforma', (error) => {
+    user_number = tmpClientUser.match(/tmpClient_(\d+)/);
+    user_section = document.title.split(" | ");
+    console.log(user_section);
+    client.publish('conn_channel', 'User '+user_number[1]+' en '+user_section[1], (error) => {
         console.log(error || 'Mensaje enviado !!')
     })
 })
-
 client.on('message', (topic, message) => {
     console.log('Mensaje recibido bajo el tópico ', topic,' -> ', message.toString())
 })
-
 client.on('reconnect', (error) => {
     console.log('reconnecting:', error)
 })
-
 client.on('error', (error) => {
     console.log('Connection failed:', error)
-})*/
- 
+})
